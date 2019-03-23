567ed5bc5e1f58995ec9c518d220e7c1d4cdc3e0
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-23 23:03:45 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 require_relative "../lib/student.rb"
-
+require_relative './db/seeds.rb'
 Student.create(name: "Melissa", grade: "10th")
 Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+namespace :db do
+  desc 'seed the database with some dummy data'
+  task :seed do
+end
+end
+end
